4b04767c15bd87c205cd0a9c86bcdac2beea0fde
thumbd78492 <79299981+thumbd78492@users.noreply.github.com>
2022-07-22 22:35:45 +0800

    Create 78. Subsets.cpp

---
+++ Medium/78. Subsets.cpp
@@ -0,0 +1,23 @@
+set<vector<int>> ans;
+
+void DFS(vector<int>& nums, vector<int>& nowVec, int now)
+{
+    if (now == nums.size())
+    {
+        ans.insert(nowVec);
+        return;
+    }
+    nowVec.push_back(nums[now]);
+    DFS(nums, nowVec, now+1);
+    nowVec.pop_back();
+    DFS(nums, nowVec, now+1);
+    return;
+}
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<int> vec;
+    DFS(nums, vec, 0);
+    vector<vector<int>> ret;
+    ret.assign(ans.begin(), ans.end());
+    return ret;
+}
